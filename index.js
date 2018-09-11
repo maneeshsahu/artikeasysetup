f64231b993851fba5704dd6bb751e91c99ef2107
Maneesh Sahu-SSI <maneesh.sahu@ssi.samsung.com>
2018-09-11 16:15:51 -0700

    Fix default routes

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ var express = require("express"),
     bodyParser = require('body-parser'),
     port = process.env.PORT || 3000;
 
-
-
 // Express Middleware
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require('./api/routes/easySetupRoutes');
+routes(app); // register the easy setup routes       
+
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-routes(app); // register the easy setup routes       
 app.listen(port, () => {
     console.log("ARTIK Easy Setup Server Running on port " + port);
 });
