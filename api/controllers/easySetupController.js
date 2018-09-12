fbdc26b1c7a802086383b6950ab7d858426c469c
Maneesh Sahu-SSI <maneesh.sahu@ssi.samsung.com>
2018-09-11 17:15:03 -0700

    Moving to controller

--- api/controllers/easySetupController.js
+++ api/controllers/easySetupController.js
@@ -1,5 +1,29 @@
 'use strict';
-var wifi = require('../models/easySetupModel')
+// Wi-Fi Setup
+var connman = require('connman-simplified')();
+var wifi;
+
+function initConnman() {
+    if (!wifi) {
+        connman.init(function(err) {
+            if (err) {
+                console.error('Error initializing connman ' + JSON.stringify(err));
+            }
+            connman.initWiFi(function(err, wifiL, properties) {
+                if (err) {
+                    console.error("Error initializing WiFI " + JSON.stringify(err));
+                }
+                wifi = wifiL;
+                // Cache the WiFi Networks
+                wifi.getNetworks(function(err, list) {
+                    console.log("networks: ", wifi.getServicesString(list));
+                });
+            }); 
+        });
+    }
+}
+
+initConnman();
 
 exports.sayHello = function(req, res) {
     var response = {
--- api/models/easySetupModel.js
+++ api/models/easySetupModel.js
@@ -1,29 +1,6 @@
 'use strict';
 
-// Wi-Fi Setup
-var connman = require('connman-simplified')();
-var wifi;
-
-function initConnman() {
-    if (!wifi) {
-        connman.init(function(err) {
-            if (err) {
-                console.error('Error initializing connman ' + JSON.stringify(err));
-            }
-            connman.initWiFi(function(err, wifiL, properties) {
-                if (err) {
-                    console.error("Error initializing WiFI " + JSON.stringify(err));
-                }
-                wifi = wifiL;
-                // Cache the WiFi Networks
-                wifi.getNetworks(function(err, list) {
-                    console.log("networks: ", wifi.getServicesString(list));
-                    return wifi;
-                });
-            }); 
-        });
-    }
-}
 
 
-module.exports = initConnman();
+
+module.exports = {};
