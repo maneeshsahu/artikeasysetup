5bdeb3e5684b3702a8909066d35eddee3c28d95a
Maneesh Sahu-SSI <maneesh.sahu@ssi.samsung.com>
2018-09-14 16:15:36 -0700

    AP, Station Modes

--- api/controllers/easySetupController.js
+++ api/controllers/easySetupController.js
@@ -6,46 +6,66 @@ const mac1Address = process.env.MAC1_ADDRESS
 const artik = require('artik-sdk');
 const settings = require('../../settings')
 var wifi = artik.wifi;
+var wifi_station,
+    wifi_ap;
 var cachedWifiNetworks = {};
 
 var actions_button = settings.button, 
 	actions_led = settings.led;
-
 var button = new artik.gpio(actions_button, 'button', 'in', 'rising', 0);
 var led  = new artik.gpio(actions_led, 'led', 'out', 'none', 0);
 
 var ledState = 1;
-function toggleLED () {
+function startAP () {
 	led.request();
 	led.write(ledState);
 	led.release();
 	
 	console.log('setLED( ' + actions_led + ') value: ' + ledState);
-	ledState ^= 1;
+    ledState ^= 1;
+    
+    wifi_ap = wifi.wifi_ap();
+    wifi_ap.on('started', function() {
+        var ssid = 'ARTIK_' + mac1Address;
+        var pwd = '';
+        var channel = 1;
+        var openmode = true;
+
+        console.log("Starting Wi-Fi Access Point");
+        console.log("SSID : '" + ssid + "'");
+        console.log("Pass : '" + pwd + "'");
+        console.log("Channel : '" + channel + "'");
+        console.log("OpenMode : '" + openmode + "'");
+        var res = wifi_ap.start_ap(ssid, pwd, channel, openmode ?
+                wifi_ap.WIFI_ENCRYPTION_OPEN : wifi_ap.WIFI_ENCRYPTION_WPA2);
+        console.log("Result => '" + res + "'");
+    });
+}
+
+function startStation() {
+    wifi_station = wifi.wifi_station();
+    wifi_station.on('started', function() {
+        console.log(wifi_station.get_info());
+        wifi_station.scan_request();
+    });
+    
+    wifi_station.on('connected', function() {
+        console.log('connected');
+    });
+    
+    wifi_station.on('scan', function(list) {
+        var results = JSON.parse(list);
+        console.log("Scanned WiFi Networks: " + list);
+        cachedWifiNetworks = results;
+    });
 }
 
 button.on('changed', function (value) {
 	console.log("button pressed");
-	toggleLED();
+	startAP();
 });
 button.request();
 
-
-var wifi_station = new wifi.wifi_station();
-wifi_station.on('started', function() {
-	console.log(wifi_station.get_info());
-	wifi_station.scan_request();
-});
-
-wifi_station.on('connected', function() {
-	console.log('connected');
-});
-
-wifi_station.on('scan', function(list) {
-	var results = JSON.parse(list);
-	console.log("Scanned WiFi Networks: " + list);
-    cachedWifiNetworks = results;
-});
 
 exports.sayHello = function(req, res) {
     var response = {
--- settings.js
+++ settings.js
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2017 Samsung Electronics Co., Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict'
 const artik = require('artik-sdk');
 
@@ -24,34 +8,34 @@ const settings = {
 
 function getButton() {
 	switch (artik.get_platform_name().toLowerCase()) {
-		case 'Artik 710':
+		case 'artik 710':
 			// SW403
 			return 30;
-		case 'Artik 530':
+		case 'artik 530':
 			return 30;	
-		case 'Artik 520':
+		case 'artik 520':
 			return 121;
-		case 'Artik 1020':
+		case 'artik 1020':
 			return 8;
 		default:
-			console.log("Invalid Platform name: " + artik.get_platform_name());
+			console.log("Invalid Platform name: " + artik.get_platform_name().toLowerCase());
 	    	process.exit(-1);
 	}	
 }
 
 function getLED() {
 	switch (artik.get_platform_name().toLowerCase()) {
-		case 'Artik 710':
+		case 'artik 710':
 			return 28;
-		case 'Artik 530':
+		case 'artik 530':
 			//
 			return 28;
-		case 'Artik 520':
+		case 'artik 520':
 			return 135;
-		case 'Artik 1020':
+		case 'artik 1020':
 			return 22;
 		default: 
-			console.log("Invalid Platform name: " + artik.get_platform_name());
+			console.log("Invalid Platform name: " + artik.get_platform_name().toLowerCase());
 	    	process.exit(-1);
 	}
 }
